06b0e472829d63b7624f49a697ba51065f1bce5f
com-luke <luke@pulsation.com.tw>
2015-08-24 18:40:51 +0800

    all change!!

---
+++ local/app/Http/Controllers/Controller/Demo01.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+use Redirect, Input;
+
+
+class Demo01Controller extends Controller
+{
+
+    public function index()
+    {
+
+        echo "demo_01";
+
+    }
+
+}
